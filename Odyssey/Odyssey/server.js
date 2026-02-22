5666dfcce17b96a3cc2c2b9bd29f4c45c4cbc626
eduardoloz <eddieslozano@gmail.com>
2026-02-22 02:27:58 -0500

    CORS

--- Odyssey/Odyssey/server.js
+++ Odyssey/Odyssey/server.js
@@ -29,6 +29,13 @@ const readline  = require('readline');
 const path      = require('path');
 
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+});
 app.use(express.json());
 
 // name -> BotState
